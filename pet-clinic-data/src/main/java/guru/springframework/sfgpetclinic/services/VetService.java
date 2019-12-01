7e3473155be1d0fa85a80d89f0084a453c611cfe
lucascalsilva <lucasc.alm.silva@gmail.com>
2019-12-01 13:55:55 +0100

    Adding base common interface. Closes #11

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -1,10 +1,11 @@
 package guru.springframework.sfgpetclinic.services;
 
+import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudServices<Vet, Long> {
 
     Vet findById(Long id);
     Vet save(Vet vet);
