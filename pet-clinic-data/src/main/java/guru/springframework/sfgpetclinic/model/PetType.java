6d14f9ef502e5c9e6d90088d9eaa35c06dfdc5a0
lucascalsilva <lucasc.alm.silva@gmail.com>
2020-02-17 22:23:04 +0100

    Added specialty entity and its relation with vet. Closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,14 +1,5 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class PetType extends BaseEntity {
+public class PetType extends NamedEntity {
 
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
