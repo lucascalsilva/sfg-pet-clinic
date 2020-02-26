b21885b03de042dfedb7accd010232dc5fcb9a80
lucascalsilva <lucasc.alm.silva@gmail.com>
2020-02-26 05:28:31 +0100

    Fixed broken links. Closes #46

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
+@RequestMapping({"/owners.html", "/owners", "/owners/index", "/owners/index.html"})
 public class OwnerController {
 
     private final OwnerService ownerService;
@@ -17,9 +17,14 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/owners.html", "/owners"})
+    @RequestMapping
     public String listOwners(Model model){
         model.addAttribute("owners", ownerService.findAll());
         return "owners/owners";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners(Model model){
+        return "notimplemented";
     }
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping({"/vets", "/vets.html", "/vets/index", "/vets/index.html"})
 public class VetController {
 
     private final VetService vetService;
@@ -14,7 +15,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets.html"})
+    @RequestMapping
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/vets";
