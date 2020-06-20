509c59a0552b5479e29e4cac337eac38a4bdd26d
lucascalsilva <lucasc.alm.silva@gmail.com>
2020-06-20 11:32:40 +0200

    Added vets rest endpoint feature. Closes #65.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,21 +1,30 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
-@RequestMapping({"/vets", "/vets.html", "/vets/index", "/vets/index.html"})
 @RequiredArgsConstructor
 public class VetController {
 
     private final VetService vetService;
 
-    @RequestMapping
+    @GetMapping({"/vets", "/vets.html", "/vets/index", "/vets/index.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/vets";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
